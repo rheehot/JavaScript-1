c7e89195bcf7ba2bbe327f36069220052dfb9c4f
또르 <hansol416@naver.com>
2019-06-01 10:21:58 +0900

    충격...

---
+++ 1-4.js
@@ -0,0 +1,18 @@
+// hoisting
+if (true){
+    let a = 10 
+    if (true){
+        console.log(10) // 여기는 10이 찍히겠고
+        const a = 20
+    }
+    console.log(a) // 두번째로 여기는 20이 찍히겠지
+}
+console.log(a)  // 우선 내 생각대로라면 여기는 안찍혀야하고
+
+/*
+< 출력된 값 >
+10
+10
+~~/1-4.js:10
+console.log(a)  
+*/
