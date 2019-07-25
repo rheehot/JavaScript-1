149dece2d10985b4cde1e39b91e8924e205c98a1
또르 <hansol416@naver.com>
2019-07-25 19:46:49 +0900

    조건문(if)

---
+++ JavaScript200/3_if.js
@@ -0,0 +1,8 @@
+var result = true;
+if (result) console.log ("result가 참입니다.");
+if (!result)
+    console.log('실행 안됌')
+if (result) {
+    console.log("result의 결과 : ")
+    console.log(">> 참 입니다.")
+}
