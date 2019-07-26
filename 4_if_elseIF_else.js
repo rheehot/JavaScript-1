bce0de6dbed6f210fd0e5d696c7a46b62626b866
또르 <hansol416@naver.com>
2019-07-26 17:55:23 +0900

    다중 if 사용

---
+++ 4_if_elseIF_else.js
@@ -0,0 +1,10 @@
+var number = 2;
+if (number==1){
+    console.log("number은 "+ number +" 입니다.");
+} else if (number == 2) {
+    console.log("number은 "+ number +" 입니다.");
+} else if (number == 3){
+    console.log("number은 "+ number +" 입니다.")
+} else {
+    console.log("numbersms 1,2,3 중 해당하는 것이 없습니다.")
+}
