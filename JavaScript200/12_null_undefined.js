ffdef7d45e8b2c03fae0f2699b92d27e3556071e
또르 <hansol416@naver.com>
2019-07-27 12:21:40 +0900

    내용 추가

--- JavaScript200/12_null_undefined.js
+++ JavaScript200/12_null_undefined.js
@@ -1,8 +1,10 @@
 // null
 var value = null; //비어있는, 존재하지 않는 값
 console.log(value);
-console.log(typeof value);
+console.log(typeof value); // object라고 뜨는데 객체가 아니라고 함..
 // undefined
 var value2; // 아무 값도 할당받지 않은 상태
 console.log(value2);
 console.log(typeof value2)
+
+// 같은 value 변수로 했을때 null object 로 뜸!..
