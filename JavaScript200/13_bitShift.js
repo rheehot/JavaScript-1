ad17d7409b5356cf0d52118fe3bf57e6eb414140
또르 <hansol416@naver.com>
2019-07-27 14:35:11 +0900

    bitshift

---
+++ JavaScript200/13_bitShift.js
@@ -0,0 +1,7 @@
+console.log(14 & 11); // AND 연산자
+console.log(~14); // NOT 연산자
+console.log(14|11); // OR 연산자
+console.log(14 ^ 11); // XOR 연산자
+console.log(2 << 3); //우측의 수만큼 왼쪽으로 이동하고 오른쪽은 0으로 채움
+console.log(14 >> 1); // 우측의 수만큼 우측으로 이동 남은 비트 버림 
+console.log(14>>>2); // 우측의 수만큼 오른쪽으로 이동하고 좌측은 0으로 채움
