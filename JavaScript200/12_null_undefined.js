f7fd9756742d84b23be11be6d3bb5e83f4d97075
또르 <hansol416@naver.com>
2019-07-27 12:08:33 +0900

    Null & Undefined

---
+++ JavaScript200/12_null_undefined.js
@@ -0,0 +1,8 @@
+// null
+var value = null; //비어있는, 존재하지 않는 값
+console.log(value);
+console.log(typeof value);
+// undefined
+var value2; // 아무 값도 할당받지 않은 상태
+console.log(value2);
+console.log(typeof value2)
