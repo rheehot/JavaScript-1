95dffd253211654047bb601edc6f9f7966445f63
9992 <hansol416@naver.com>
2019-08-09 09:11:25 +0900

    function

---
+++ JavaScript200/20_function_basic.js
@@ -0,0 +1,10 @@
+var greeting_expression = function(name) {
+    console.log("hi  " + name);
+}
+
+function greeting(name){
+    console.log("hi  " + name);
+}
+
+greeting_expression("hansol")
+greeting("hansol")
