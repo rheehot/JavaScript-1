cb4425abb3ba6aa36d1e8008f67229e9bab953fa
9992 <hansol416@naver.com>
2019-08-10 22:52:45 +0900

    예외처리 공부

--- JavaScript200/etc_test1.js
+++ JavaScript200/etc_test1.js
@@ -1,13 +1,23 @@
 function test(name){
     if (typeof name !== 'string') throw 'Error Test';
     console.log(name+"은 string 타입이 맞습니다.")
+    return name;
 }
 
+function test2(name){
+    console.log(name + "값의 예외처리를 정상적으로 마쳤습니다.")
+}
+
+var name;
+
+// 에러가 발생하면, catch에서 끝내고
+// 에러가 발생하지 않았다면 test2함수 실행됌 ( finally로 넘어간다는 말 )
 try{
-    test("한솔");
-    test(123456);
+    name = test("한솔");
+    name = test(123456);
 } catch(e){
     console.log("error : " +e);
 } finally {
-    console.log("완료")
+    test2(name);
+    // console.log("완료")
 }
