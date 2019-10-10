0596c697f83626280e4509c938c816e3754bdfad
9992 <hansol416@naver.com>
2019-10-10 20:33:11 +0900

    using const

---
+++ JavaScript200/42_using_const.js
@@ -0,0 +1,10 @@
+const URL = 'http://외부.com';
+// URL = 'http://변경됨.com'; // 변경 안됨
+console.log(URL)
+
+if (true) {
+    const URL2 = 'http://내부.com';
+    console.log(URL2)
+}
+
+// console.log(URL2); // 정의 되지 않음
