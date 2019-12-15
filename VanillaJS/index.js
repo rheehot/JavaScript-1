807989fc47c1ab4372b18361ad849e6300d47b53
9992 <hansol416@tu.ac.kr>
2019-12-15 23:50:55 +0900

    js practice

--- VanillaJS/index.js
+++ VanillaJS/index.js
@@ -1,14 +1,22 @@
 const title = document.querySelector("#title");
-let status = 0; // 전역에다 변수 하나를 선언하고 이를 통해 값을 제어하게 만듬
 
-function handleClick(event){
-    status += 1;
-    // 클릭시 홀수로 바뀌니 홀수 일때는 파랑, 한번 더 클릭시에는 하얀색으로 되게끔 
-    if(status % 2 == 1){
-        title.style.color = "blue";
-    } else {
-        title.style.color = "white";
+const BASE_COLOR = "#rgb(52,73,94)";
+const OTHER_COLOR = "#000";
+
+function handleClick() {
+    const currentColor = title.style.color;
+    if (currentColor === BASE_COLOR){
+        title.style.color = OTHER_COLOR;
+        console.log(title.style.color)
+    }else{
+        title.style.color = BASE_COLOR;
+        console.log(title.style.color)
     }
 }
 
-title.addEventListener("click", handleClick);   
+function init(){
+    title.style.color = BASE_COLOR;
+    title.addEventListener("click", handleClick);
+}
+
+init();
